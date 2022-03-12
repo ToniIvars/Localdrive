91619e470052cbb93bfbd82a5623c9f381e29f4d
ToniIvars <toniivaju@gmail.com>
2022-03-12 12:00:35 +0100

    Add create_user api endpoint

--- api/main.py
+++ api/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from api.utils import db
+from .schemas import UserCreate
+
 app = FastAPI()
 
 
@@ -9,4 +12,13 @@ async def version():
     return {
         'name': 'Local Drive API',
         'version': '1.0',
+    }
+
+@app.post('/create-user')
+async def create_user(user: UserCreate):
+    db.create_user(**user.dict())
+
+    return {
+        'status': 'success',
+        'message': f'User {user.name} created successfully'
     }
