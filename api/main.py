ea834d4a0e72fb0c950cabf6ace24d0be0e30e3c
ToniIvars <toniivaju@gmail.com>
2022-03-12 13:00:13 +0100

    Add some tags and metadata for the docs

--- api/main.py
+++ api/main.py
@@ -3,7 +3,18 @@ from fastapi import FastAPI, Header, HTTPException
 from api.utils import db
 from .schemas import UserCreate, UserDelete
 
-app = FastAPI()
+description = '''
+This is a cloud service at home. You can:
+- Create your own user
+- Upload files and remove them
+- Delete your user when you want (**this will remove all your files**)
+'''
+
+app = FastAPI(
+    title='Local Drive API',
+    description=description,
+    version='1.0'
+)
 
 def check_token(token: str):
     if not db.valid_token(token):
@@ -11,15 +22,15 @@ def check_token(token: str):
 
 # API endpoints
 
-@app.get('/')
-@app.get('/version')
-async def version():
+@app.get('/', tags=['Information'])
+@app.get('/info', tags=['Information'])
+async def info():
     return {
         'name': 'Local Drive API',
         'version': '1.0',
     }
 
-@app.post('/create-user')
+@app.post('/create-user', tags=['Users'])
 async def create_user(user: UserCreate):
     new_user_token = db.create_user(**user.dict())
 
@@ -28,7 +39,7 @@ async def create_user(user: UserCreate):
         'token': new_user_token
     }
 
-@app.post('/delete-user')
+@app.post('/delete-user', tags=['Users'])
 async def delete_user(user: UserDelete, token: str = Header(..., alias='API_TOKEN')):
     check_token(token)
 
@@ -38,7 +49,7 @@ async def delete_user(user: UserDelete, token: str = Header(..., alias='API_TOKE
         'detail': f'User deleted successfully',
     }
 
-@app.get('/me')
+@app.get('/me', tags=['Users'])
 async def me(token: str = Header(..., alias='API_TOKEN')):
     check_token(token)
 
