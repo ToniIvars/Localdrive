00a18d27b4c76c97c3c3b2085e9f6cc7e6c8e5a2
ToniIvars <toniivaju@gmail.com>
2022-03-14 20:32:09 +0100

    Add upload_file test

--- api/tests.py
+++ api/tests.py
@@ -11,6 +11,8 @@ HEADERS = {
     'Content-Type': 'application/json'
 }
 
+FILE_HEADERS = {'accept': 'application/json'}
+
 API_TOKEN = ''
 
 def test_create_user():
@@ -28,6 +30,8 @@ def test_create_user():
     API_TOKEN = response.json()['token']
 
 def test_get_user_token():
+    global HEADERS, FILE_HEADERS
+
     post_data = {
         "username": "fake_user",
         "password": "fake_user_pass"
@@ -37,16 +41,23 @@ def test_get_user_token():
     assert response.status_code == 200
     assert response.json()['token'] == API_TOKEN
 
+    HEADERS.update({'API_TOKEN': API_TOKEN})
+    FILE_HEADERS.update({'API_TOKEN': API_TOKEN})
+
+def test_upload_file():
+    file_to_upload = {'post_file': open('api/upload_test_file.txt' ,'rb')}
+
+    response = client.post('/upload-file?path=test', files=file_to_upload, headers=FILE_HEADERS)
+
+    assert response.status_code == 200
+    assert response.json()['detail'] == "File uploaded successfully"
+
 def test_delete_user():
     post_data = {
         "password": "fake_user_pass"
     }
 
-    headers = HEADERS
-    headers.update({
-        'API_TOKEN': API_TOKEN
-    })
-    response = client.post('/delete-user', json=post_data, headers=headers)
+    response = client.post('/delete-user', json=post_data, headers=HEADERS)
 
     assert response.status_code == 200
     assert response.json()['detail'] == "User deleted successfully"
