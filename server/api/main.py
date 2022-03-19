7f6c7b594a604331f5eb20a1a7f121cc2a998137
ToniIvars <toniivaju@gmail.com>
2022-03-19 13:28:12 +0100

    Add CORS middleware to allow the frontend to communicate with the API

--- server/api/main.py
+++ server/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Header, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, Response
 
 from api.utils import db, file_handling
@@ -15,6 +16,19 @@ app = FastAPI(
     title='Local Drive API',
     description=description,
     version='1.0'
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Extra functions
