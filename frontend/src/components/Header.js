f18fe789698819ff341498a17bef441ab7d87c8f
ToniIvars <toniivaju@gmail.com>
2022-03-20 16:40:22 +0100

    Change h1 hiding with useEffect to a media query

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,12 @@
-import { useEffect, useState } from 'react'
 import UploadButton from './UploadButton'
 import logo from '../logo.svg'
 
 const Header = () => {
-  const [bigWidth, setBigwidth] = useState(window.innerWidth > 500)
-
-  const handleResize = () => setBigwidth(window.innerWidth > 500)
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  })
-
   return (
     <header id="navbar">
       <div id="logo-container">
         <img src={logo} alt="Logo" id="logo" />
-        {bigWidth && <h1>Local Drive</h1>}
+        <h1>Local Drive</h1>
       </div>
       <UploadButton />
     </header>
