63ffadc588de0227f5802b29329896269e546adf
ToniIvars <toniivaju@gmail.com>
2022-03-22 22:48:12 +0100

    Add notification component and show one when mkdir

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Api from './api/Api'
 import Header from './components/Header'
 import CardContainer from './components/CardContainer'
 import { MkdirForm } from './components/Forms'
+import Notification from './components/Notification'
 
 const apiToken = process.env.REACT_APP_USER_TOKEN
 const api = new Api(apiToken)
@@ -12,6 +13,7 @@ function App() {
   const [dirItems, setDirItems] =  useState([])
   const [actualPath, setActualPath] = useState('')
   const [form, setForm] = useState('')
+  const [notification, setNotification] = useState({})
 
   // List dir
   const listDir = () => api.listDir(actualPath).then(json => setDirItems(json))
@@ -34,15 +36,20 @@ function App() {
     api.mkdir(actualPath, dirName)
       .then(data => {
         console.log(data.detail)
+        setNotification({message: data.detail, status: 'success'})
         listDir()
       })
-      .catch(e => console.log(e.detail))
+      .catch(e => {
+        console.log(e.detail)
+        setNotification({message: e.detail, status: 'error'})
+      })
   }
 
   return (
     <>
       {form === 'mkdir' && <MkdirForm makeDirectory={makeDirectory} setForm={setForm} />}
       <Header setForm={setForm} />
+      {Object.keys(notification).length !== 0 && <Notification message={notification.message} status={notification.status} setNotification={setNotification} />}
       <CardContainer items={dirItems} actualPath={actualPath} setActualPath={setActualPath} downloadItem={downloadItem} />
     </>
   );
---
+++ frontend/src/components/Notification.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { BsCheckLg, BsXLg } from 'react-icons/bs'
+
+const Notification = ({ message, status, setNotification }) => {
+  const [animation, setAnimation] = useState('back-in')
+
+  const getIcon = () => {
+    if (status === 'success') {
+      return <BsCheckLg className='notification-icon' />
+    } else {
+      return <BsXLg className='notification-icon' />
+    }
+  }
+
+  const cancel = () => {
+    setAnimation('back-out')
+    setTimeout(() => setNotification({}), 600);
+  }
+
+  return (
+    <div className={`notification notification-${status} ${animation}`} onClick={cancel}>{getIcon()}{message}</div>
+  )
+}
+
+export default Notification
